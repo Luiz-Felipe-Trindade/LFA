b8bebfae6afb46644b78b78dcff8b44508a641f2
Luiz-Felipe-Trindade <135871914+Luiz-Felipe-Trindade@users.noreply.github.com>
2025-12-10 13:13:31 -0300

    Inciado o desenvolvimento do conversor de BNF para AFN

--- src/bnf.py
+++ src/bnf.py
@@ -9,3 +9,35 @@ def constructor(lista_producoes):
         return bnf
     except Exception as e:
         return (f"Erro ao construir a BNF: {e}")
+
+def bnf_to_afn(bnf):
+    afn={}
+    estados_finais = set()
+    
+    try:
+        for simbolo, producoes in bnf.items():
+            if simbolo not in afn:
+                afn[simbolo] = {}
+                
+            for p in producoes:
+                
+                # Tratamento para produção vazia (ε)
+                if p == "ε":
+                    estados_finais.add(simbolo)
+                    continue
+                
+                # Tratamento para transições com terminal + não terminal (aB)
+                if len(p) == 2:
+                    estado_atual = p[0]
+                    proximo_estado = p[1]
+                    
+                    afn[simbolo].setdefault(estado_atual, set()).add(proximo_estado)
+                 
+                    
+                    
+                estados = p.split()
+                afn[simbolo].append(estados)
+        return afn
+    except Exception as e:
+        return (f"Erro ao converter BNF para AFN: {e}")
+    
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import file_reader as fr 
-import bnf
+import bnf as b
 
 print("Iniciando a leitura do arquivo...")
 lista_producoes = fr.file_reader('exemplo1.txt')
 
 if lista_producoes == None:
     print("Nenhuma linha foi lida do arquivo.")
-else:
-    print(f"{len(lista_producoes)} linhas lidas do arquivo.")
-    print("Construindo a BNF...")
-    bnf = bnf.constructor(lista_producoes)
-    print(bnf)
+
+print(f"{len(lista_producoes)} linhas lidas do arquivo.")
+print("Construindo a BNF...")
+bnf = b.constructor(lista_producoes)
+print(bnf)
+
+afn = b.bnf_to_afn(bnf)
+print("BNF convertida para AFN:")
+print(afn)
